ff0921a9ded804382e2793bcbe648b36fc097fa5
Kevin Espinoza <kcesp96@gmail.com>
2022-02-25 18:18:40 -0800

    yes master

--- Test.cpp
+++ Test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,6 +8,20 @@ int main(){
     cout << "Hello FIza!" << endl;
     cout << "lel kek" << endl;
 
+    int size = 100;
+    vector<int> v (0, 1);
+
+    for (int i = 2; i < size; i++)
+    {
+        v.push_back(v[i-1] + v[i - 2]);
+    }
+
+    for (int num: v){
+        cout << num << "; " ;
+    }
+
+    cout << endl;
+
     cout << 2 + 2;
-    return 
+    return 0;
 }
